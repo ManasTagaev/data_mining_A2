bcf47a071ae6ab4001b3dae202bc3eae6171db24
Aibek Minbaev <aibekminbaev050402@gmail.com>
2024-05-31 12:00:57 +0900

    done: upgraded experiment 2

--- k-meanspp-experiments/experiment-2.py
+++ k-meanspp-experiments/experiment-2.py
@@ -3,38 +3,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
-# Load data from CSV
 column_names = ['point', 'x', 'y', 'cluster']
 data = pd.read_csv('artset1.csv', names=column_names)
-# data = pd.read_csv('artd-31.csv', names=column_names)
-# data = pd.read_csv('test.csv', names=column_names)
 
+grouped_data = data.groupby('cluster')
 
-# Extract coordinates
+cluster_data_list = []
+
+for _, group_data in grouped_data:
+    cluster_data_list.append(group_data[['x', 'y']].values)
+
+cluster_data_array = cluster_data_list #3np.array(cluster_data_list)
+
 coordinates = data[['x', 'y']].values
 
-def plot(data, centroids, title):
-    plt.scatter(data[:, 0], data[:, 1], marker='.', color='gray', label='data points')
-    plt.scatter(centroids[:, 0], centroids[:, 1], color='red', label='selected centroids')
-    # plt.scatter(centroids[-1, 0], centroids[-1, 1], color='red', label='next centroid')
-    plt.title(title)
+
+def plot(ax, centroids, title):
+    num_clusters = len(cluster_data_array)
+    colors = plt.cm.rainbow(np.linspace(0, 1, num_clusters))
     
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    for i in range(num_clusters):
+        cluster_data = cluster_data_array[i]
+        ax.scatter(cluster_data[:, 0], cluster_data[:, 1], marker='.', color=colors[i], label=f'Cluster {i}')
+    
+    ax.scatter(centroids[:, 0], centroids[:, 1], color='black', marker='x', label='Centroids')
+    ax.set_title(title)
+    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
+    all_points = np.concatenate(cluster_data_array, axis=0)
     padding = 10000
-    plt.xlim(min(data[:, 0]) - padding, max(data[:, 0]) + padding)
-    plt.ylim(min(data[:, 1]) - padding, max(data[:, 1]) + padding)
-   
-    # print(min(data[:, 0]), max(data[:, 0]))
-    # print(min(data[:, 1]), max(data[:, 1]))
+    ax.set_xlim(all_points[:, 0].min() - padding, all_points[:, 0].max() + padding)
+    ax.set_ylim(all_points[:, 1].min() - padding, all_points[:, 1].max() + padding)
 
-    plt.show()
 
-def distance(p1, p2):
-    return np.sqrt(np.sum((p1 - p2)**2))
-
-# Init for k-means++ 
-def initialize(data, k):
+def initialize(data, k, ax):
     centroids = []
     centroids.append(data[np.random.randint(data.shape[0]), :])
 
@@ -52,18 +54,20 @@ def initialize(data, k):
         next_centroid = data[np.argmax(dist), :]
         centroids.append(next_centroid)
         dist = []
-    # print(np.array(centroids))
-    plot(data, np.array(centroids), 'k-means++ Initialization')
+
+    plot(ax, np.array(centroids), 'k-means++ Initialization')
     return centroids
 
-
-#Init for k-means 
-def initialize_random(data, k):
+def initialize_random(data, k, ax):
     centroids = data[np.random.choice(data.shape[0], k, replace=False)]
-    # print(np.array(centroids))
-    plot(data, np.array(centroids),'k-means Initialization')
+    plot(ax, np.array(centroids),'k-means Initialization')
     return centroids
 
-#Call init functions for k-means and k-means++ 
-centroids_random = initialize_random(coordinates, k=15)
-centroids = initialize(coordinates, k=15)
+def distance(p1, p2):
+    return np.sqrt(np.sum((p1 - p2)**2))
+
+fig, axs = plt.subplots(2, 1, figsize=(8, 12))
+centroids_random = initialize_random(coordinates, k=15, ax=axs[0])
+centroids = initialize(coordinates, k=15, ax=axs[1])
+plt.tight_layout()
+plt.show()
